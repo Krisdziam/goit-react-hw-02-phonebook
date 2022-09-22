a5ffe7144a14dd4c8232b07e54db437f82d9cb30
Kristian <kris.dziamka@gmail.com>
2022-09-22 23:03:55 +0300

    fff

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-
-import React, { Component } from 'react'
-
+import { nanoid } from 'nanoid';
+import React, { Component } from 'react';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  nameId = nanoid();
 
+  handleInputChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+}
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      name: '',
+    })
+  }
 
-  render(){
-    
+  render() {
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.nameId}>Name</label>
+          <input
+            onChange={this.handleInputChange}
+            id={this.nameId}
+            type="text"
+            name="name"
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button>Add Contact</button>
+        </form>
+      </>
+    );
   }
-  }
+}
